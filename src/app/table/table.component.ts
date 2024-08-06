b3de7dfe760321d705f5a4ee9098c5fffec451d1
Fredrick Yalley <94568827+yalleyfred@users.noreply.github.com>
2024-08-06 17:49:32 +0000

    feat: add change detection

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
