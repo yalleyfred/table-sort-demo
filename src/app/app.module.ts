fe9ce8eee48273f9742f55c62d3888cffafc18a8
Fredrick Yalley <94568827+yalleyfred@users.noreply.github.com>
2024-08-06 15:24:06 +0000

    feat: utilize http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
